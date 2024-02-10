14f3d78733a13f51a7198889506f7d91f820376f
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-02-10 17:49:05 +0100

    Aplikacja CarApi - ćwiczymy resta - klasa serwisowa

---
+++ src/main/java/com/example/carapi/service/CarService.java
@@ -0,0 +1,18 @@
+package com.example.carapi.service;
+
+import com.example.carapi.model.Car;
+import com.example.carapi.repository.CarRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CarService {
+    private final CarRepository carRepository;
+
+    public CarService(CarRepository carRepository) {
+        this.carRepository = carRepository;
+    }
+
+    public void addCar(Car car) {
+        carRepository.save(car);
+    }
+}
