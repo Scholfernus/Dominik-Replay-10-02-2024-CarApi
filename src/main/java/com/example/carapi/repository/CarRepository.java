8d4e3cfd4ee84aaa66dabed430c027fb6b7b12c4
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-02-13 19:32:03 +0100

    Aplikacja CarApi - ćwiczymy ApiRest - repository

--- src/main/java/com/example/carapi/repository/CarRepository.java
+++ src/main/java/com/example/carapi/repository/CarRepository.java
@@ -1,9 +1,19 @@
 package com.example.carapi.repository;
 
-import com.example.carapi.model.Car;
+import com.example.carapi.model.CarModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface CarRepository extends JpaRepository<Car, Long> {
+public interface CarRepository extends JpaRepository<CarModel, Long> {
+
+    List<CarModel> findByColor(String color);
+
+    @Query("SELECT c FROM CarModel c WHERE c.color = :color")
+    List<CarModel> listCarsByColor(@Param("color") String color);
+
 }
