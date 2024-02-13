e3112abf21c7b044ee64ae9f844bec68293ea343
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-02-13 19:31:16 +0100

    Aplikacja CarApi - ćwiczymy ApiRest - dto - data transfer object

---
+++ src/main/java/com/example/carapi/dto/CarDto.java
@@ -0,0 +1,12 @@
+package com.example.carapi.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CarDto {
+    private String brand;
+    private String model;
+    private String color;
+}
