6c6fd9ce09e4c67309d872f451d652fc1ae81e39
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-02-13 19:32:24 +0100

    Aplikacja CarApi - ćwiczymy ApiRest - klasa serwisowa

--- src/main/java/com/example/carapi/service/CarService.java
+++ src/main/java/com/example/carapi/service/CarService.java
@@ -1,8 +1,13 @@
 package com.example.carapi.service;
 
-import com.example.carapi.model.Car;
+import com.example.carapi.dto.CarDto;
+import com.example.carapi.model.CarModel;
 import com.example.carapi.repository.CarRepository;
+import com.example.carapi.utils.CarMapper;
+import com.example.carapi.utils.error.CarNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CarService {
@@ -12,7 +17,31 @@ public class CarService {
         this.carRepository = carRepository;
     }
 
-    public void addCar(Car car) {
+    public void addCar(CarModel car) {
         carRepository.save(car);
+    }
+    public List<CarModel> getCarList() {
+        return carRepository.findAll();
+    }
+
+    public CarModel getCarById(Long id) {
+        return carRepository.findById(id).orElseThrow(() -> new RuntimeException("empty"));
+    }
+
+    public List<CarModel> getCarsByColor(String color) {
+        return carRepository.listCarsByColor(color);
+    }
+
+    public CarDto updateCarColor(Long id, CarDto carDto) {
+        CarModel car = carRepository.findById(id)
+                .orElseThrow(() -> new CarNotFoundException("Car not found"));
+                car.setColor(carDto.getColor());
+        CarModel newModel = carRepository.save(car);
+        return CarMapper.toCarDto(newModel);
+
+    }
+
+    public void remove(Long id) {
+        carRepository.deleteById(id);
     }
 }
