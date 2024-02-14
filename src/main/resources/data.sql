f02100676a542e3f75f15c993edb0390bc25f7c7
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-02-14 22:15:41 +0100

    Aplikacja CarApi - ćwiczymy ApiRest - właściwości

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO car_model (brand, model, color) VALUES ('Ford', 'Mustang', 'Red');
+INSERT INTO car_model (brand, model, color) VALUES ('Opel', 'Astra', 'White');
+INSERT INTO car_model (brand, model, color) VALUES ('Kia', 'Soren', 'Yellow');
